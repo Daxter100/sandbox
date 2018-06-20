5810c3c0372ac2ea74ebe5df8eef18ebfd28a4dc
Daxter100 <39886450+Daxter100@users.noreply.github.com>
2018-06-20 17:18:56 +0300

    V 0 (#2)
    * setup trial ignore
    
    * first boss checkpoint
    
    * setup lnk ignore
    
    * Last prep for debug deploy

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 var builder = require('botbuilder');
 var os = require('os');
+var spawn = require('child_process').execFile;
 
-/*
+function pwr(script) {
+  spawn('powershell.exe', [script])
+}
+
 var restify = require('restify');
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
-*/
 
-var connector = new builder.ConsoleConnector().listen();
-/*
+
+//var connector = new builder.ConsoleConnector().listen();
+
+
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
 
 server.post('/api/messages', connector.listen());
-*/
 
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s, in %s, %s.", session.message.text, os.platform(), os.arch());
-    
+  if (/shut down/i.test(session.message.text)) {
+    pwr('stop-computer');
+    //pwr('get-childitem E:\\sandbox app.js -recurse');
+  }
+  else{
+    session.send('You said: %s.', session.message.text, os.platform(), os.arch());
+  }
+
 });
